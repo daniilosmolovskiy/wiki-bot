3eb4edc7e3a327719a8159dc30535068a2fdea47
Daniil Osmolovskiy <danielosmolovskiy@gmail.com>
2020-08-13 13:36:59 +0300

    done

--- index.js
+++ index.js
@@ -1,29 +1,26 @@
 const express = require('express');
-require('dotenv').config();
-
 const { Telegram } = require('telegraf');
-
-const port = process.env.PORT || 3000;
-
 const tg = new Telegram(process.env.BOT_TOKEN);
 
-const getArticle = require('./getArticle');
+require('dotenv').config();
+
+const port = process.env.PORT || 3000;
+const chat = '-1001342903377';
 const app = express();
+
+const getArticle = require('./getArticle');
 
 app.get('/', async (req, res) => {
   const fullArticle = await getArticle();
-
-  tg.sendPhoto(process.env.CHANNEL_ID, `https:${fullArticle.imageSrc}`);
-
-  tg.sendMessage(process.env.CHANNEL_ID, fullArticle.text.join('\n \n').trim(), {
+  tg.sendPhoto(chat, `https:${fullArticle.imageSrc}`);
+  tg.sendMessage(chat, fullArticle.text.join('\n \n').trim(), {
     reply_markup: {
       inline_keyboard: [
         [{ text: "Полная статья", url: `https://ru.wikipedia.org/${fullArticle.articleLink}` }]
       ]
     }
   });
-
   res.send("Bot is working!");
 })
 
-app.listen(port)
+app.listen(port);
